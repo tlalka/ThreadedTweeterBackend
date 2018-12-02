bd8c618f3b801305a802dfc01a2d5f05830b1d26
peakay <peakaylopolis@gmail.com>
2018-12-02 15:59:14 -0500

    basic rollback tests

--- threaded_tweeter_backend/tests/test_data/example_tweets.py
+++ threaded_tweeter_backend/tests/test_data/example_tweets.py
@@ -1,4 +1,6 @@
 import json
+from ...app import APIException
+
 tweet_1 = {'input': json.dumps({
             'TWEETS':[
                 {
@@ -9,3 +11,17 @@ tweet_1 = {'input': json.dumps({
             ]
             }),
             'output': [{'body': 'hello', 'id': 'a random tweet id'}]}
+
+fail_tweet_1 = {'input': json.dumps({
+                'TWEETS':[
+                    {
+                        'STATUS':'hello world',
+                        'MEDIA':[]
+                    },
+                    {  
+                        'STATUS': 'THROW AN ERROR',
+                        'MEDIA': []
+                    }
+                ]
+                }),
+                'output': {'errorMessage': 'Post Error: \nTweets rolled back.'}}
--- threaded_tweeter_backend/tests/test_post_thread.py
+++ threaded_tweeter_backend/tests/test_post_thread.py
@@ -3,7 +3,7 @@ import pytest
 from .test_base import BaseTestCase
 import json
 from mock import Mock, patch
-from ..app import app
+from ..app import app, APIException
 import twitter
 from .test_data import example_tweets
 
@@ -12,12 +12,26 @@ class MockTwitterApi:
     def __init__(self, *a, **kw):
         pass
     def PostUpdate(self, status, **kw):
-        return MockStatus(status, "a random tweet id")
+        if status == 'THROW AN ERROR':
+            raise Exception
+        return MockStatus(status)
+    def VerifyCredentials(self):
+        return MockUser("a fake user", 123)
+    def GetReplies(self, head_tweet, trim_user):
+        return [MockStatus("hello")]
+    def DestroyStatus(self, status_id):
+        pass
 
 class MockStatus:
-    def __init__(self, status, tweet_id):
+    def __init__(self, status, tweet_id="a random tweet id"):
         self.text = status
         self.id = tweet_id
+        self.user = MockUser("a fake user", 124)
+
+class MockUser:
+    def __init__(self, username, user_id):
+        self.username = username
+        self.id = user_id
 
 class TestPostThread(BaseTestCase):
     def test_post_thread(self):
@@ -26,6 +40,13 @@ class TestPostThread(BaseTestCase):
             for test in test_cases:
                 assert self.make_post(test['input']) == test['output']
     
+    def test_rollback(self):
+        test_cases = [example_tweets.fail_tweet_1]
+        with patch.object(twitter, 'Api',  MockTwitterApi):
+            for test in test_cases:
+                assert self.make_post(test['input']) == test['output']
+
+
     def make_post(self, tweet_json):
         self.client.set_cookie('.threadedtweeter.com', 'access_token_key', 'test123')
         self.client.set_cookie('.threadedtweeter.com', 'access_token_secret', 'test146')
